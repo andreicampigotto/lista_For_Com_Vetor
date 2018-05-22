57068d37537bd744d1527597e60373b4d5b7c127
andreicampigotto <andreicampigotto@gmail.com>
2018-05-22 17:22:20 -0300

    lsita for com vetor
    até a 3

---
+++ listaForVetor/listaForVetor/Exercicio02.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace listaForVetor
+{
+    class Exercicio02
+    {
+        public Exercicio02()
+        {
+            int[] idades = new int[7];
+
+            Console.Write("Guardar idades: ");
+            for (int i = 0; i < idades.Length; i++)
+            {
+                Console.Write("\nIdade: ");
+                idades[i] = Convert.ToInt32(Console.ReadLine());
+                //Console.Clear();
+            }
+        }
+    }
+}
---
+++ listaForVetor/listaForVetor/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace listaForVetor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //new Exercicio01();
+            //new Exercicio02();
+            new Exercicio03();
+        }
+    }
+}
