bf989a862c1a29f9bfab7c96cfd53108b2254f19
andreicampigotto <andreicampigotto@gmail.com>
2018-05-25 17:24:44 -0300

    salvando começo da 7
    salvando

---
+++ listaForVetor/listaForVetor/Exercicio07.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace listaForVetor
+{
+    class Exercicio07
+    {
+        public Exercicio07()
+        {
+            string[] caracteres = new string[20];
+            string[] especiaisCaracteres = new string[caracteres.Length];
+            string[] consoantes = new string[caracteres.Length];
+            string[] vogais = new string[caracteres.Length];
+            int qtdaVogais = 0, qtdaConsoantes = 0, contador = 0;
+            
+            Console.Write("Digite uma frase: " +
+                          "\nmáximo 20 caracteres");
+
+            for (int i = 0; i < caracteres.Length; i++)
+            {
+                caracteres[i] = Console.ReadLine().ToUpper().Trim();
+            }
+            for (int i = 0; i < caracteres.Length; i++)
+            {
+                
+            }
+
+        }
+    }
+}
